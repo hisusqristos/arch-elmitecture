1e2bfcb757f201e40c8eafe65abb1565acc2675d
hisusqristos <varyadaser@gmail.com>
2024-01-03 23:53:58 +0400

    :wind-whime: Context text, better symetry

--- allergies/src/Main.elm
+++ allergies/src/Main.elm
@@ -3,11 +3,11 @@ module Main exposing (..)
 import Allergies
 import Browser
 import Html exposing (Html, br, div, input, text)
-import Html.Attributes exposing (placeholder, src, style, value, width)
+import Html.Attributes exposing (placeholder, style, value)
 import Html.Events exposing (onInput)
 import List
 import Maybe exposing (withDefault)
-import String exposing (replace, toInt)
+import String exposing (toInt)
 
 
 main =
@@ -42,36 +42,68 @@ toPresentable =
 view : Model -> Html Msg
 view model =
     div
-        [ style "display" "flex"
+        [ style "display" "grid"
         , style "justify-content" "center"
+        , style "align-content" "center"
+        , style "grid-auto-flow" "row"
+        , style "gap" "20px"
+        , style "align-items" "center"
+        , style "justify-items" "center"
         , style "background-image" "url('background.jpg')"
         , style "opacity" "0.4"
         , style "height" "1300px" -- consider to changing to screen size
         ]
-        [ div
-            [ style "align" "center" ]
-            [ allergiesText
-            , aranjviPaiskavik model
-            ]
+        [ allergiesText
+        , paiskavik model
+        , describtion
         ]
 
 
 allergiesText =
-    div [ style "font-size" "1300%", style "margin-bottom" "7%", style "margin-top" "300px" ] [ text "Allergies" ]
+    div [ style "font-size" "1300%", style "margin-bottom" "3%" ] [ text "Allergies" ]
 
 
-aranjviPaiskavik : Model -> Html Msg
-aranjviPaiskavik model =
+paiskavik : Model -> Html Msg
+paiskavik model =
     div
-        [ style "font-size" "50px"
+        [ style "font-size" "325%"
         , style "display" "flex"
         , style "flex-direction" "column"
-        , style "margin-left" "25%"
-        , style "padding" "20px"
-        , style "background-color" "#ff9800"
-        , style "border-radius" "7%"
-        , style "width" "50%"
+        , style "padding" "2.5% 7% 2.5% 7%"
+        , style "background-color" "#2E8BC0"
+        , style "border" "2px solid #000000"
+        , style "border-radius" "25px"
+        , style "width" "30%"
         ]
         [ input [ placeholder "score", value (Debug.toString model), onInput Change ] []
-        , text (toPresentable model)
+        , div [] [ text (toPresentable model) ]
         ]
+
+
+describtion : Html msg
+describtion =
+    div
+        [ style "margin-top" "5%"
+        , style "font-weight" "1000"
+        , style "border-style" "dashed"
+        , style "padding" "3%"
+        , style "background-color" "#FFFFFF"
+        , style "opacity" "0.8"
+        ]
+        (List.intersperse (br [] []) <|
+            [ text "Enter a person's allergy score, determine their full list of allergies."
+            , text "An allergy test produces a single numeric score which contains the information about all the allergies the person has."
+            , text "The list of items (and their value) that were tested are:"
+            , text " "
+            , text "- eggs (1)"
+            , text "- peanuts (2)"
+            , text "- shellfish (4)"
+            , text "- strawberries (8)"
+            , text "- tomatoes (16)"
+            , text "- chocolate (32)"
+            , text "- pollen (64)"
+            , text "- cats (128)"
+            , text " "
+            , text "So if Tom is allergic to peanuts and chocolate, he gets a score of 34."
+            ]
+        )
