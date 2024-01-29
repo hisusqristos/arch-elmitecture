fb0156d9d1d4587c494f90c37595a6f72d824abc
hisusqristos <varyadaser@gmail.com>
2024-01-30 01:44:04 +0400

    :axe: rename ouija to talking board for simplicity

--- secure-treasure-chest/src/TalkingBoard.elm
+++ secure-treasure-chest/src/TalkingBoard.elm
@@ -1,3 +1,48 @@
+module TalkingBoard exposing (..)
+
+import Html exposing (Html, div, text)
+import Html.Attributes exposing (style)
+import Simple.Animation as Animation exposing (Animation)
+import Simple.Animation.Animated as Animated
+import Simple.Animation.Property as P
+import Svg
+import Svg.Attributes exposing (cx, cy, fill, r, stroke, strokeWidth)
+
+
+esoterics : List (Html msg)
+esoterics =
+    [ div
+        [ style "background-color" "#E0115F"
+        , style "border" "2px solid black"
+        , style "border-radius" "25px"
+        , style "width" "55%"
+        , style "padding" "2.5% 5% 0% 5%"
+        , style "text-align" "center"
+        , style "line-height" "2"
+        ]
+        [ talkingBoard alphabet
+        , div [ style "font-size" "30px" ] [ text "⛤" ]
+        , wanderingCircle
+        ]
+    ]
+
+
+type alias Location =
+    { x : Float
+    , y : Float
+    }
+
+
+alphabet : List String
+alphabet =
+    [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ]
+
+
+talkingBoard : List String -> Html msg
+talkingBoard =
+    List.intersperse " " >> List.foldr (++) "" >> text
+
+
 wanderingCircle : Html msg
 wanderingCircle =
     let
